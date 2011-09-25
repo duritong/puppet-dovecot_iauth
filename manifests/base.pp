76e5a22a8122e6cf67c206fd6168a6e2d77f9cbf
o <o@immerda.ch>
2011-09-25 16:57:18 +0200

    fix gem includes

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,7 +1,6 @@
 class dovecot-iauth::base {
     require rubygems::activesupport
     require rubygems::postgres
-    require rubygems::digest
 
     package { 'dovecot-iauth':
         ensure => present,
