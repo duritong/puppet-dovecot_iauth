dcbbf6cb8561e23cd41eb8370190cdec4d6722b1
o <o@immerda.ch>
2011-09-25 16:41:20 +0200

    add needed gems

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,8 @@
 class dovecot-iauth::base {
+    require rubygems::active_support
+    require rubygems::postgres
+    require rubygems::digest
+
     package { 'dovecot-iauth':
         ensure => present,
     }
