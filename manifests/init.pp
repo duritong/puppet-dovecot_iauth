01969d3106644d6b30988850b412c869faac81c8
mh <mh@immerda.ch>
2015-06-26 14:01:04 +0200

    modernize the module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,25 @@
-# manifests/init.pp - module to manage dovecot iauth
-
+# module to manage dovecot iauth
 class dovecot_iauth {
-    include dovecot_iauth::base
+  include rubygems::bcrypt
+
+  file{
+    '/usr/libexec/dovecot/checkpassword-bcrypt' :
+      ensure  => directory,
+      require => Package['dovecot'],
+      owner   => root,
+      group   => root,
+      mode    => '0644';
+    '/usr/libexec/dovecot/checkpassword-bcrypt/checkpassword-bcrypt.rb':
+      source  => 'puppet:///modules/dovecot_iauth/checkpassword-bcrypt.rb',
+      before  => Service['dovecot'],
+      owner   => root,
+      group   => root,
+      mode    => '0755';
+    '/usr/libexec/dovecot/checkpassword-bcrypt/checkpassword-bcrypt.incl.rb':
+      source  => 'puppet:///modules/dovecot_iauth/checkpassword-bcrypt.incl.rb',
+      before  => Service['dovecot'],
+      owner   => root,
+      group   => root,
+      mode    => '0755';
+  }
 }
