5b58c4de438eec6f61f0bf3ed28724ba37de49ae
mh <mh@immerda.ch>
2018-07-07 00:16:06 +0200

    add package dependencies

--- manifests/trees.pp
+++ manifests/trees.pp
@@ -1,5 +1,6 @@
 # trees helper for authentication
 class dovecot_iauth::trees {
+  ensure_packages(['rubygem-ffi','libsodium'])
   file{
     '/usr/local/trees':
       ensure => directory,
@@ -7,9 +8,10 @@ class dovecot_iauth::trees {
       group  => 0,
       mode   => '0755';
     '/usr/local/trees/trees.rb':
-      source => 'puppet:///modules/dovecot_iauth/trees/trees.rb',
-      owner  => root,
-      group  => 0,
-      mode   => '0644';
+      source  => 'puppet:///modules/dovecot_iauth/trees/trees.rb',
+      owner   => root,
+      group   => 0,
+      mode    => '0644',
+      require => Package['rubygem-ffi','libsodium'];
   }
 }
